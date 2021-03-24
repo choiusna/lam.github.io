065f49ed23a0a5dc4fee230cc6fa6f0a5743d07b
Seung Geol Choi <choi@usna.edu>
2021-03-24 18:47:53 -0400

    .

--- home.js
+++ home.js
@@ -12,7 +12,7 @@ function startsection(s, b)
 {
   str = '<table class=noborder width=100%>\
     <tr valign=top>\
-    <td style="background-color:#FFD378; width:18%;">\
+    <td style="background-color:#FFD378; width:20%;">\
     <br><center>';
 
   document.write(str);
@@ -21,7 +21,7 @@ function startsection(s, b)
   document.write(s);
   document.write("<br><span style=width:100%></span>")
   str2 = '</center>\
-    <td style="width:5%">\
+    <td style="width:3%">\
     <td style="width:77%">';
   document.write(str2)
 }
